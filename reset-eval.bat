c7d34af1b92dbdcfe16cbf3041029f32a7d3c4cf
dprofe07 <69401363+dprofe07@users.noreply.github.com>
2023-02-08 23:11:29 +0300

    Update reset-eval.bat

--- reset-eval.bat
+++ reset-eval.bat
@@ -1,5 +1,5 @@
 @echo off
-REM Delete eval folder with licence key and options.xml which contains a reference to it
+
 for %%I in (WebStorm, IntelliJ, CLion, Rider, GoLand, PhpStorm, Resharper, PyCharm) do (
     for /d %%a in ("%USERPROFILE%\.%%I*") do (
         rd /s /q "%%a/config/eval"
@@ -11,7 +11,6 @@ for %%I in (WebStorm, IntelliJ, CLion, Rider, GoLand, PhpStorm, Resharper, PyCha
     reg delete "HKEY_CURRENT_USER\Software\JavaSoft\Prefs\jetbrains\%%I" /f
 )
 
-REM Delete registry key and jetbrains folder (not sure if needet but however)
 
 
 reg delete "HKEY_CURRENT_USER\Software\JavaSoft\Prefs\/Jet/Brains./User/Id/On/Machine" /f
